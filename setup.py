e6efc1ac5e55701f529f0d7cf074e1c0ca5dffd6
Ryan Georgi <rgeorgi@gmail.com>
2017-01-31 17:57:26 -0600

    Adding setuptools script.

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='riples_classifier',
+      version='0.1',
+      description='Unified classifier framework for RiPLes project code.',
+      install_requires=['scikit-learn>=0.18'])
